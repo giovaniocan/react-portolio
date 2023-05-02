e3172eedfd272e316c8100a17fa78e947207fb0f
giovani <giovaniocan@gmail.com>
2023-05-02 15:47:17 -0300

    add info of projects

---
+++ src/utils/projects.ts
@@ -0,0 +1,20 @@
+import igniteShopPng from '../../public/IgniteShop.png'
+import CoffeDeliveryPng from '../../public/coffeeDelivery.png'
+
+const IgniteShop = {
+  title: 'Ignite Shop',
+  description: 'Site para venda de itens do Ignite.',
+  techs: ['React', 'TypeScript', 'Next', 'Stitches', 'Stripe'],
+  image: igniteShopPng,
+  github: 'https://github.com/giovaniocan/04-iginite-shop',
+}
+
+const CoffeDelivery = {
+  title: 'Coffee Delivery',
+  description: 'Site paragerenciar um carrinho de compras',
+  techs: ['React', 'TypeScript', 'Styled-components'],
+  image: CoffeDeliveryPng,
+  github: 'https://github.com/giovaniocan/Coffee-Delivery',
+}
+
+export { CoffeDelivery, IgniteShop }
