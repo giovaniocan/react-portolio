f0f1e3f786166468452dd4cd625393387cb05025
giovani <giovaniocan@gmail.com>
2023-05-03 13:42:31 -0300

    add name for the page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,10 +5,12 @@ export function Projects() {
   return (
     <div id="projects" className=" w-full flex items-center flex-col ">
       <h1 className=" mt-24 text-4xl leading-10 font-medium maxw ">Projetos</h1>
-      <div className=" mt-10 flex items-center justify-center gap-12 flex-wrap ">
+      <div className=" mt-10 flex items-center justify-center gap-12 flex-wrap  ">
         <ProjectCard project={CoffeDelivery} />
         <ProjectCard project={IgniteShop} />
         <ProjectCard project={CoffeDelivery} />
+        <ProjectCard project={IgniteShop} />
+        <ProjectCard project={IgniteShop} />
         <ProjectCard project={IgniteShop} />
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,20 @@ import { Header } from '@/components/Header'
 import { Presentation } from '@/components/Presentation'
 import { About } from '@/components/About'
 import { Projects } from '@/components/Projects'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className=" w-11/12 max-w-6xl my-0 mx-auto py-8 ">
-      <Header />
-      <Presentation />
-      <About />
-      <Projects />
-    </div>
+    <>
+      <Head>
+        <title>Portifólio</title>
+      </Head>
+      <div className=" w-11/12 max-w-6xl my-0 mx-auto py-8 ">
+        <Header />
+        <Presentation />
+        <About />
+        <Projects />
+      </div>
+    </>
   )
 }
