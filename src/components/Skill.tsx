fdbb53beedfd2e506e1b4e526a9bd3500eab8621
giovani <giovaniocan@gmail.com>
2023-05-03 17:28:20 -0300

    skill if sinished nowww

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -14,7 +14,7 @@ export function Skill({ src, title, level }: Props) {
   return (
     <div className=" group w-36 py-8 flex items-center justify-center bg-gray-200 border-solid border rounded-sm border-gray-400  dark:bg-zinc-800/75 dark:border-zinc-700  ">
       <Image src={src} width={80} height={80} alt="" />
-      <div className="absolute hidden h-20 mt-20 w-36 mb-3 bg-zinc-950 pl-4 justify-center flex-col group-hover:flex group-hover:visible">
+      <div className="absolute hidden h-20 mt-20 w-[calc(144px-2px)] mb-4 bg-zinc-100 pl-4 justify-center flex-col group-hover:flex group-hover:visible dark:bg-zinc-900">
         <h3 className=" text-zinc-400/70 text-lg font-bold ">{title}</h3>
         <div className="flex">
           {Array.from(Array(starsCollored), (e, i) => (
