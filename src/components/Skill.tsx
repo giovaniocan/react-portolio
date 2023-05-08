8d3567488cb5e84af1b357679a8d849194a028ca
giovani <giovaniocan@gmail.com>
2023-05-08 14:55:15 -0300

    add animation in the skill component

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import { Star } from 'phosphor-react'
+import { useAnimation, motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
 
 interface Props {
   src: string
@@ -11,8 +14,28 @@ export function Skill({ src, title, level }: Props) {
   const starsCollored = level
   const starsEmpty = 5 - starsCollored
 
+  const controls = useAnimation()
+  const [ref, inView] = useInView()
+
+  const containerVariants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+    hidden: { opacity: 0, y: -200 },
+  }
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
+  }, [controls, inView])
+
   return (
-    <div className=" group w-36 py-8 flex items-center justify-center bg-gray-200 border-solid border rounded-sm border-gray-400  dark:bg-zinc-800/75 dark:border-zinc-700  ">
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={containerVariants}
+      className=" group w-36 py-8 flex items-center justify-center bg-gray-200 border-solid border rounded-sm border-gray-400  dark:bg-zinc-800/75 dark:border-zinc-700  "
+    >
       <Image src={src} width={80} height={80} alt="" />
       <div className="absolute hidden h-20 mt-20 w-[calc(144px-2px)] mb-4 bg-zinc-100 pl-4 justify-center flex-col group-hover:flex group-hover:visible dark:bg-zinc-900">
         <h3 className=" text-zinc-400/70 text-lg font-bold ">{title}</h3>
@@ -25,6 +48,6 @@ export function Skill({ src, title, level }: Props) {
           ))}
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
