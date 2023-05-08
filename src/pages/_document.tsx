0dbae296dd54413ea1f6e619e1c2225b7581ba09
giovani <giovaniocan@gmail.com>
2023-05-08 15:24:13 -0300

    add favicon to the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/favicon-16x16.png"
+          type="image/x-icon"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
